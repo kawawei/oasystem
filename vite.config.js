7effb9f7a9cd94d21f0f7d1d0c1b7036709db0bc
kawawei <weihomeben@gmail.com>
2024-12-02 08:50:21 +0800

    fix: 更新 Vercel 部署配置

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: '_assets'
   }
 }) 
